161ee7a921233d33439aefb8ccaf7ba35cb40310
Pierre Fromager <pierre@debnovo.pier-infor.fr>
2019-10-14 17:50:32 +0200

    test/nymfonia : init Controllers test Api V1 Auth

---
+++ tests/Controllers/Api/V1/AuthTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase as PFT;
+use App\Config;
+use App\Kernel;
+use App\Container;
+use App\Controllers\Api\V1\Auth;
+
+/**
+ * @covers \App\Controllers\Api\V1\Auth::<public>
+ */
+class ApiV1ControllerAuthTest extends PFT
+{
+
+    const TEST_ENABLE = true;
+    const CONFIG_PATH = '/../../../../config/';
+
+    /**
+     * config
+     *
+     * @var Config
+     */
+    protected $config;
+
+    /**
+     * container
+     *
+     * @var Container
+     */
+    protected $container;
+
+    /**
+     * instance
+     *
+     * @var Kernel
+     */
+    protected $instance;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        if (!self::TEST_ENABLE) {
+            $this->markTestSkipped('Test disabled.');
+        }
+        $this->config = new Config(
+            Config::ENV_CLI,
+            __DIR__ . self::CONFIG_PATH
+        );
+        $this->container = new Container(
+            $this->config->getSettings(Config::_SERVICES)
+        );
+        $this->instance = new Auth($this->container);
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+        $this->instance = null;
+        $this->container = null;
+        $this->config = null;
+    }
+
+    /**
+     * get any method from a class to be invoked whatever the scope
+     *
+     * @param String $name
+     * @return void
+     */
+    protected static function getMethod(string $name)
+    {
+        $class = new \ReflectionClass(Auth::class);
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        unset($class);
+        return $method;
+    }
+
+    /**
+     * testInstance
+     * @covers App\Controllers\Api\V1\Auth::__construct
+     */
+    public function testInstance()
+    {
+        $this->assertTrue($this->instance instanceof Auth);
+    }
+}
