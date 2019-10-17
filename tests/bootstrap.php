99f886a5807aebecf4ed77dcbfaa79902e2826f4
Pierre Fromager <pierre@debnovo.pier-infor.fr>
2019-10-17 13:06:09 +0200

    coverage/nymfonia : change composer phpdbg path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 error_reporting(0);
 ini_set('display_errors', 0);
-ini_set('display_errors', 0);
 ini_set('log_errors', 1);
 ini_set('date.timezone', 'Europe/Paris');
 ini_set('register_globals', 0);
