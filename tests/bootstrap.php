5dc66c9542115c6104b466f376c4630eece4ef2a
Pierre Fromager <pierre@debnovo.pier-infor.fr>
2019-10-17 12:49:02 +0200

    test/nymfonia : disable errors output to let phpdbg run coverage nicely

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+error_reporting(0);
+ini_set('display_errors', 0);
+ini_set('display_errors', 0);
 ini_set('log_errors', 1);
 ini_set('date.timezone', 'Europe/Paris');
 ini_set('register_globals', 0);
