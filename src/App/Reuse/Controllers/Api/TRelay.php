6a20c5870c6d3e9928a34daaaa8310d3a65d5081
Pierre Fromager <pierre@debnovo.pier-infor.fr>
2019-10-27 15:13:59 +0100

    bugfix/nymfonia : TRelay

--- src/App/Reuse/Controllers/Api/TRelay.php
+++ src/App/Reuse/Controllers/Api/TRelay.php
@@ -27,38 +27,44 @@ trait TRelay
     protected function apiRelayRequest(string $method, string $url, array $headers = [], $datas = [])
     {
         $cha = curl_init();
-        curl_setopt($cha, CURLOPT_VERBOSE, false);
-        curl_setopt($cha, CURLOPT_URL, $url);
-        curl_setopt($cha, CURLOPT_POST, ($method == Request::METHOD_POST));
-        curl_setopt($cha, CURLOPT_TIMEOUT, 300);
-        curl_setopt($cha, CURLOPT_USERAGENT, self::USER_AGENT);
-        curl_setopt($cha, CURLOPT_BUFFERSIZE, self::BUFFER_SIZE);
-        curl_setopt($cha, CURLOPT_HTTPHEADER, $headers);
-        if ($this->apiRelayOptionHeader) {
-            curl_setopt($cha, CURLOPT_VERBOSE, 1);
-            curl_setopt($cha, CURLOPT_HEADER, 1);
+        if (false !== $cha) {
+            curl_setopt($cha, CURLOPT_VERBOSE, false);
+            curl_setopt($cha, CURLOPT_URL, $url);
+            curl_setopt($cha, CURLOPT_POST, ($method == Request::METHOD_POST));
+            curl_setopt($cha, CURLOPT_TIMEOUT, 300);
+            curl_setopt($cha, CURLOPT_USERAGENT, self::USER_AGENT);
+            curl_setopt($cha, CURLOPT_BUFFERSIZE, self::BUFFER_SIZE);
+            curl_setopt($cha, CURLOPT_HTTPHEADER, $headers);
+            if ($this->apiRelayOptionHeader) {
+                curl_setopt($cha, CURLOPT_VERBOSE, 1);
+                curl_setopt($cha, CURLOPT_HEADER, 1);
+            }
+            if ($method == Request::METHOD_POST && $datas) {
+                curl_setopt(
+                    $cha,
+                    CURLOPT_POSTFIELDS,
+                    http_build_query($datas)
+                );
+            }
+            curl_setopt($cha, CURLOPT_RETURNTRANSFER, 1);
+            $result = curl_exec($cha);
+            $error = (false === $result);
+            $this->apiRelayResponse = ($error) ? 'Api relay error' : $result;
+            $curlInfoCode = ($error) ? 500 : curl_getinfo($cha, CURLINFO_HTTP_CODE);
+            $this->apiRelayHttpCode = ($curlInfoCode === false) ? 500 : $curlInfoCode;
+            if ($this->apiRelayOptionHeader) {
+                $this->apiRelayHeaders = [];
+                $headerSize = curl_getinfo($cha, CURLINFO_HEADER_SIZE);
+                $rawHeaders = substr($this->apiRelayResponse, 0, $headerSize);
+                $this->apiRelayHeaders = explode("\r\n\r\n", $rawHeaders, 2);
+                $this->apiRelayResponse = substr(
+                    $this->apiRelayResponse,
+                    $headerSize
+                );
+            }
+            if (false === $error) {
+                curl_close($cha);
+            }
         }
-        if ($method == Request::METHOD_POST && $datas) {
-            curl_setopt(
-                $cha,
-                CURLOPT_POSTFIELDS,
-                http_build_query($datas)
-            );
-        }
-        curl_setopt($cha, CURLOPT_RETURNTRANSFER, 1);
-        $this->apiRelayResponse = curl_exec($cha);
-        $curlInfoCode = curl_getinfo($cha, CURLINFO_HTTP_CODE);
-        $this->apiRelayHttpCode = ($curlInfoCode === false) ? 500 : $curlInfoCode;
-        if ($this->apiRelayOptionHeader) {
-            $this->apiRelayHeaders = [];
-            $headerSize = curl_getinfo($cha, CURLINFO_HEADER_SIZE);
-            $rawHeaders = substr($this->apiRelayResponse, 0, $headerSize);
-            $this->apiRelayHeaders = explode("\r\n\r\n", $rawHeaders, 2);
-            $this->apiRelayResponse = substr(
-                $this->apiRelayResponse,
-                $headerSize
-            );
-        }
-        curl_close($cha);
     }
 }
