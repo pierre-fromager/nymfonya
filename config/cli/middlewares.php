530952ee3aa69955649d90820efeefd4eb189478
Pierre Fromager <pierre@debnovo.pier-infor.fr>
2019-11-02 16:46:28 +0100

    refacto/nymfonya : Middleware Cors move headers to config

--- config/cli/middlewares.php
+++ config/cli/middlewares.php
@@ -1,10 +1,28 @@
 <?php
 
+use App\Http\Request;
+use App\Http\Headers;
+
 return [
     \App\Middlewares\Cors::class => [
         'enabled' => true,
         'prefix' => '/api/v1/',
         'exclude' => ['stat/filecache'],
+        'headers' => [
+            Headers::HEADER_ACA_ORIGIN  =>  '*',
+            Headers::HEADER_ACA_CREDENTIALS => 'true',
+            Headers::HEADER_ACA_METHODS => implode(',', [
+                Request::METHOD_GET, Request::METHOD_POST,
+                Request::METHOD_PUT, Request::METHOD_DELETE,
+                Request::METHOD_OPTIONS
+            ]),
+            Headers::HEADER_ACA_HEADERS =>  implode(',', [
+                'Access-Control-Allow-Headers', 'Origin', 'Accept',
+                'X-Requested-With', 'Content-Type', 'Access-Control-Request-Method',
+                'Access-Control-Request-Headers', 'Access-Control-Allow-Origin',
+                'Authorization'
+            ])
+        ]
     ],
     \App\Middlewares\Jwt::class => [
         'enabled' => true,
--- src/App/Middlewares/Cors.php
+++ src/App/Middlewares/Cors.php
@@ -61,20 +61,7 @@ class Cors implements ILayer
                     ])
                     ->getHeaderManager()
                     ->add(Headers::CONTENT_TYPE, 'application/json; charset=utf-8')
-                    ->addMany([
-                        Headers::HEADER_ACA_ORIGIN  =>  '*',
-                        Headers::HEADER_ACA_CREDENTIALS => 'true',
-                        Headers::HEADER_ACA_METHODS => implode(',', [
-                            Request::METHOD_GET, Request::METHOD_POST, Request::METHOD_PUT,
-                            Request::METHOD_DELETE, Request::METHOD_OPTIONS
-                        ]),
-                        Headers::HEADER_ACA_HEADERS =>  implode(',', [
-                            'Access-Control-Allow-Headers', 'Origin', 'Accept',
-                            'X-Requested-With', 'Content-Type', 'Access-Control-Request-Method',
-                            'Access-Control-Request-Headers', 'Access-Control-Allow-Origin',
-                            'Authorization'
-                        ])
-                    ]);
+                    ->addMany($this->configParams['headers']);
             }
         }
     }
