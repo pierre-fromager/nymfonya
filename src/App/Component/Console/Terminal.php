1c9958846a4ff95b17e5b8f0da8ec7cfc060d5a5
Pierre Fromager <pierre@debnovo.pier-infor.fr>
2019-12-07 13:44:49 +0100

    debug/nymfonya : Component Console Terminal

--- src/App/Component/Console/Terminal.php
+++ src/App/Component/Console/Terminal.php
@@ -5,6 +5,11 @@ namespace App\Component\Console;
 use App\Component\Console\Dimensions;
 use App\Component\Console\Process;
 
+/**
+ * Console terminal is a terminal for console.
+ * 
+ * @todo A lot of stuff
+ */
 class Terminal
 {
 
@@ -37,7 +42,6 @@ class Terminal
         $this->dimensions = new Dimensions();
         if (!$this->isWindows()) {
             $matcher = [0,0,0];
-            
             //$ttyDims = Process::readFromProcess(self::TTY_DIM_COMMAND_0);
             //echo $ttyDims;die;*/
             //var_dump($ttyDims);
@@ -46,15 +50,14 @@ class Terminal
                 $matcher = explode(self::_S, self::_S . $ttyDims);
             } else {
                 $ttyDims = Process::readFromProcess(self::TTY_DIM_COMMAND_1);
-                var_dump($ttyDims);
-
+                //var_dump($ttyDims);
                 if (preg_match('/(\d+)+;.\w{7}.(\d+)+;/', $ttyDims, $matches)) {
                     $matcher = $matches;
                 } elseif (preg_match('/;(\d+).\w{4};.(\d+)/i', $ttyDims, $matches)) {
                     $matcher = $matches;
                 }
             }
-            var_dump($matcher, $ttyDims);
+            //var_dump($matcher, $ttyDims);
             $this->dimensions->set((int) $matcher[2], (int) $matcher[1]);
         }
         return $this;
